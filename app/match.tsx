c0a130ef765f50e926118110e22c0a70b1e9e204
rokamune <reynaoaguirre@gmail.com>
2024-10-04 20:06:27 -0700

    Fetching Compatible Users (Back-End) for Match
    FIXME: compatible is currently OR to get more users for testing

--- app/match.tsx
+++ app/match.tsx
@@ -6,108 +6,77 @@ import { View, StyleSheet, SafeAreaView, Text, Image, TouchableOpacity, ScrollVi
 import BottomNavBar from '../components/BottomNavBar';
 import { Stack, useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-import { isMatch, User } from './match-algorithm'; // import isMatch and User from match-algorithm.tsx
-import { getFirestore, collection, getDoc, getDocs, query, limit, doc, where } from 'firebase/firestore';
+import { User } from './match-algorithm'; // import isMatch and User from match-algorithm.tsx
 import { getAuth } from 'firebase/auth';
 import { app } from '../firebaseConfig'; 
+import { fetchCompatibleUsers } from './match-algorithm'; // for match algorithm
+import { getFirestore, doc, updateDoc, arrayUnion } from 'firebase/firestore';
 
-
-// START of Tune of the Month UI 
-// START of Reyna Aguirre Contribution
-interface Song {
-  id: string;
-  name: string;
-  artist: string;
-  albumArt: string;
-}
-// END of Tune of the Month UI 
-// END of Reyna Aguirre Contribution
 
 // Match component definition
 const Match = () => {
-  // State to control the visibility of the match modal
-  const [showMatchModal, setShowMatchModal] = useState(false);
-  // State to manage the color of the like and dislike buttons
-  const [likeButtonColor, setLikeButtonColor] = useState('#1E1E1E');
-  const [dislikeButtonColor, setDislikeButtonColor] = useState('#1E1E1E');
+  const [showMatchModal, setShowMatchModal] = useState(false);   // State to control the visibility of the match modal
+  const [likeButtonColor, setLikeButtonColor] = useState('#1E1E1E');   // State to manage the color of the like and dislike buttons
+  const [dislikeButtonColor, setDislikeButtonColor] = useState('#1E1E1E'); // dislike button color
+  
   // Using useRouter hook to get the router instance for navigation
   const router = useRouter();
-  // Refs for animation values
+
+  // functions for animation values
   const scaleValue = useRef(new Animated.Value(0)).current;
   const opacityValue = useRef(new Animated.Value(0)).current;
-  const [tuneOfMonth, setTuneOfMonth] = useState<Song | null>(null); // for Tune of the Month
+
 
   //  START of fetch two users from Firestore
   //  START of Reyna Aguirre Contribution
-  const [user1, setUser1] = useState<User | null>(null);
-  const [user2, setUser2] = useState<User | null>(null);
+  const [user1, setUser1] = useState<User | null>(null); // current user
+  const [user2, setUser2] = useState<User | null>(null); // random user
+  const [noMoreUsers, setNoMoreUsers] = useState(false); // variable to check if no more users
+  const [compatibleUsers, setCompatibleUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const db = getFirestore(app);
-      const usersRef = collection(db, "users");
-      
-      // Fetch current user (user1)
+    
+      // fetch current user (user1)
       const auth = getAuth(app);
       const currentUser = auth.currentUser;
+      setIsLoading(true);
+
       if (currentUser) {
-        const user1Doc = await getDoc(doc(usersRef, currentUser.uid));
-        if (user1Doc.exists()) {
-          const userData = user1Doc.data() as User;
-          setUser1(userData);
-          
-          // Parse tuneOfMonth if it exists
-          if (userData.tuneOfMonth) {
-            try {
-              const parsedTuneOfMonth = JSON.parse(userData.tuneOfMonth);
-              setTuneOfMonth(parsedTuneOfMonth);
-            } catch (error) {
-              console.error('Error parsing tuneOfMonth:', error);
-              setTuneOfMonth(null);
-            }
-          } else {
-            setTuneOfMonth(null);
-          }
+        const fetchedUsers = await fetchCompatibleUsers();
+        console.log("FETCHED USERS:", fetchedUsers.map(user => user.displayName));
+        setCompatibleUsers(fetchedUsers); // fetch compatible users using the algorithm
+
+        if (fetchedUsers.length > 0) {
+          setUser1(fetchedUsers[0]);
+          setUser2(fetchedUsers.length > 1 ? fetchedUsers[1] : null);
+          setCurrentIndex(1);
+          setNoMoreUsers(false);
+        } else {
+          setNoMoreUsers(true);
         }
       }
-
-      // Fetch a random user (user2)
-      await fetchNextUser();
+      setIsLoading(false);
     };
-
     fetchUsers();
   }, []);
   //  END of fetch two users from Firestore
-  //  END of Reyna Aguirre Contribution
 
-  // Handler for when the heart button is pressed
-  const handleHeartPress = async () => {
-    if (user1 && user2) {
-      const matched = await isMatch(user1, user2);  // check if users match
-      if (matched) {
-        setLikeButtonColor('#fc6c85'); // Change to pink
-        setShowMatchModal(true);
-        animateModal(true);
+    // START of fetch a random user (user2) contribution
+    const fetchNextUser = async () => {
+      if (currentIndex < compatibleUsers.length - 1) {
+        setCurrentIndex(prevIndex => prevIndex + 1);
+        setUser2(compatibleUsers[currentIndex + 1]);
       } else {
-        console.log("No match");
-        fetchNextUser(); // Load the next user profile if no match
+        setUser2(null);
+        setNoMoreUsers(true);
       }
-    } else {
-      console.log("User data not available");
-    }
-  };
-
-  // Handler for when the message button is pressed in the modal
-  const handleMessagePress = () => {
-    // Navigate to messages screen
-    router.push('/messages');
-  };
-
-  // Handler for when the message icon button is pressed on the top right
-  const handleMessageIconPress = () => {
-    // Navigate to messages screen
-    router.push('/messages');
-  };
+    };
+    // END of fetch a random user (user2) contribution
+    // END of Reyna Aguirre Contribution
+  
 
   // Function to animate the modal
   const animateModal = (visible: boolean) => {
@@ -123,34 +92,33 @@ const Match = () => {
       }),
     ]).start();
   };
-
-  // START of Reyna Aguirre Contribution
-  const fetchNextUser = async () => {
-    const db = getFirestore(app);
-    const usersRef = collection(db, "users");
-    const auth = getAuth(app);
-    const currentUser = auth.currentUser;
-
-    if (currentUser) {
-      // Query for users that are not the current user
-      const querySnapshot = await getDocs(query(usersRef, limit(50))); // Fetch more users to increase randomness
-
-      const filteredUsers = querySnapshot.docs
-        .map((doc) => doc.data() as User)
-        .filter((user) => user.uid !== currentUser.uid); // Filter out the current user
-
-      if (filteredUsers.length > 0) {
-        // Get a random user from the filtered list
-        const randomIndex = Math.floor(Math.random() * filteredUsers.length);
-        setUser2(filteredUsers[randomIndex]);
+    
+  // Handler for when the heart button is pressed
+  const handleHeartPress = async () => {
+    if (user1 && user2) {
+      if (user1 && user2) {
+        setLikeButtonColor('#fc6c85'); // Change to pink when liked
+        setShowMatchModal(true);
+        animateModal(true);
       } else {
-        console.log("No more users to display");
-        // Handle the case when there are no more users to display
+        console.log("No match");
+        fetchNextUser(); // Load the next user profile if no match
       }
     }
   };
-  // END of Reyna Aguirre Contribution
-  
+
+  // Handler for when the message button is pressed in the modal
+  const handleMessagePress = () => {
+    // Navigate to messages screen
+    router.push('/messages');
+  };
+
+  // Handler for when the message icon button is pressed on the top right
+  const handleMessageIconPress = () => {
+    // Navigate to messages screen
+    router.push('/messages');
+  };
+
   // Effect to reset like button color when modal is closed
   useEffect(() => {
     if (!showMatchModal) {
@@ -160,21 +128,28 @@ const Match = () => {
 
   // Function to handle the close button press
   const handleClosePress = () => {
-    setDislikeButtonColor('#de3c3c'); // Change to orange
+    setDislikeButtonColor('#de3c3c'); // change to red
     fetchNextUser(); // Load the next user profile
     setTimeout(() => {
       setDislikeButtonColor('#1E1E1E'); // Change back to original color
     }, 1000);
   };
 
+  // UI rendering
   return (
     <SafeAreaView style={styles.container}>
       <Stack.Screen options={{ headerShown: false }} />
+
+      {/* User2 Profile */}
       <ScrollView 
         style={styles.content}
         contentContainerStyle={{ alignItems: 'center' }} 
       >
-        {user2 ? (
+        {isLoading ? (
+          <View>
+            <Text style={styles.message}>loading users ...</Text>
+          </View>
+        ) : user2 ? (
           <>
             <View style={styles.header}>
               <Image
@@ -194,17 +169,24 @@ const Match = () => {
                   : user2.musicPreference || 'No preferences set'}
               </Text>
             </View>
+
+  
             <View style={styles.userInfo}>
-              <Text style={styles.sectionTitle}>Tune of the Month</Text>
-              <Text style={styles.tuneOfMonth}>{user2.tuneOfMonth}</Text>
+              <Text style={styles.sectionTitle}>Favorite Performance</Text>
+              <Image
+                source={{ uri: user2.favoritePerformance }}
+                style={styles.promptImage}
+              />
             </View>
           </>
-        ) : (
+        ) : noMoreUsers ? (
           <View>
-            <Text>Loading...</Text>
+            <Text style={styles.message}>no more matches for now !</Text>
           </View>
-        )}
+        ) : null}
       </ScrollView>
+
+
 
       {/* Action buttons (like and dislike) */}
       <View style={styles.actionButtons}>
@@ -295,6 +277,15 @@ const styles = StyleSheet.create({
   },
   header: {
     alignItems: 'center',
+  },
+  message: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#000',
+    textAlign: 'center',
+    marginTop: 50,
+    marginBottom: 10,
+    fontStyle: 'italic',
   },
   profilePic: {
     width: 200,
@@ -430,8 +421,13 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     fontWeight: 'bold',
   },
+  promptImage: {
+    width: 300,
+    height: 300,
+    borderRadius: 20,
+    marginBottom: 25,
+    padding: 20,
+  },
 });
 
 export default Match;
-
-// END of Mariann Grace Dizon Contribution
