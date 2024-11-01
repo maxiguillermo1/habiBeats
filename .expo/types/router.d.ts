d179bb6af7a82012f18958dbef01aac67b2286d1
Mariann Grace Dizon <marianngrace.dizon01@student.csulb.edu>
2024-10-31 17:32:39 -0700

    Removed profilesettings.tsx

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/_sitemap` | `/ai-chatbot` | `/create-group` | `/directmessage` | `/editprofile` | `/events/event-details` | `/events/events` | `/events/myevents` | `/events/search` | `/events/trending` | `/forgot-password` | `/forgot-password-confirmation` | `/group-message` | `/landing` | `/login-signup` | `/match` | `/match-algorithm` | `/messages` | `/notification-page` | `/post-delete-survey` | `/profile` | `/profilesettings` | `/settings` | `/settings/block-list` | `/settings/change-email` | `/settings/change-password` | `/settings/current-liked-list` | `/settings/email-notifications` | `/settings/hidden-words` | `/settings/push-notifications` | `/settings/selfie-verification` | `/signup` | `/view-groups`;
+      StaticRoutes: `/` | `/_sitemap` | `/ai-chatbot` | `/create-group` | `/directmessage` | `/editprofile` | `/events/event-details` | `/events/events` | `/events/myevents` | `/events/search` | `/events/trending` | `/forgot-password` | `/forgot-password-confirmation` | `/group-message` | `/landing` | `/login-signup` | `/match` | `/match-algorithm` | `/messages` | `/notification-page` | `/post-delete-survey` | `/profile` | `/settings` | `/settings/block-list` | `/settings/change-email` | `/settings/change-password` | `/settings/current-liked-list` | `/settings/email-notifications` | `/settings/hidden-words` | `/settings/push-notifications` | `/settings/selfie-verification` | `/signup` | `/view-groups`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
