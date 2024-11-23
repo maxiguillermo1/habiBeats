df365456297458013479491f65c6152f9b5e8300
jesusdonate <66103218+jesusdonate@users.noreply.github.com>
2024-11-23 13:20:09 -0800

    Revert "Fixed navigation issue for SDK 52"
    This reverts commit cf8c355cd22a50a19f43176abeff8d9befa52720.

--- components/BottomNavBar.tsx
+++ components/BottomNavBar.tsx
@@ -65,7 +65,7 @@ const BottomNavBar = () => {
 
   // START of Jesus Donate
   // Navigate to a specific route
-  const navigateTo = (route: Href) => {
+  const navigateTo = (route: Href<string>) => {
     router.back(); // Gets rid of previous page
     router.push(route); // Navigates to the new page
   };
